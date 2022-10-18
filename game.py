d2c5a52e0de7b3d5a7c0edb1ab4f4756c10a815a
m255148 <115821522+m255148@users.noreply.github.com>
2022-10-18 09:29:48 -0400

    Added Ship, Settings

--- game.py
+++ game.py
@@ -1,1 +1,32 @@
-for i in range(10)
+import sys
+
+import pygame
+
+from settings import Settings
+
+from ship import Ship
+
+class AlienInvasion:
+
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+        self.bg_color = (self.settings.bg_color)
+
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.bg_color)
+            self.ship.blitme()
+
+            pygame.display.flip()
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+
+    def __init__(self):
+        """initializes the games settings"""
+        self.screen_width = 1200
+        self.screen_height = 600
+        self.bg_color = (40,130,230)
