ebd1651f949165f11c6791f47abcf29a92ab9086
m255148 <115821522+m255148@users.noreply.github.com>
2022-10-25 09:44:57 -0400

    Added Bullets that disapear properly

--- game.py
+++ game.py
@@ -6,14 +6,20 @@ from settings import Settings
 
 from ship import Ship
 
+from bullet import Bullet
+
 class AlienInvasion:
 
     def __init__(self):
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+
+        self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_weight = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         self.bg_color = (self.settings.bg_color)
 
 
@@ -21,6 +27,12 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+            #Getting rid of Bullets
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <=0:
+                    self.bullets.remove(bullet)
+
             self._update_screen()
     def _check_events(self):
         """Respond to keypresses and ,mouse events"""
@@ -39,14 +51,23 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+    def _fire_bullet(self):
+        """create a new bullet and add it to group"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+#SCREEN
     def _update_screen(self):
         self.screen.fill(self.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         pygame.display.flip()
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -8,3 +8,9 @@ class Settings:
 
         #ship Settings
         self.ship_speed = 1.5
+
+        #Bullet Settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
