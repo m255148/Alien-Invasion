8d7d0ba1a914bc235b34d61198ce7a359b2a2ea4
m255148 <115821522+m255148@users.noreply.github.com>
2022-11-08 09:10:04 -0500

    Finished Chapter 13

--- settings.py
+++ settings.py
@@ -8,10 +8,16 @@ class Settings:
 
         #ship Settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         #Bullet Settings
-        self.bullet_speed = 1.0
-        self.bullet_width = 3
+        self.bullet_speed = 1.5
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
         self.bullets_allowed = 3
+
+        #Alien Settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 100
+        self.fleet_direction = 1
